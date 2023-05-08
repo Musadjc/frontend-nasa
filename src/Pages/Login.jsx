68c250d6be3a9d3cfc9866a5fa36291cda167b91
Musa Djiguel <musadjiguel@gmail.com>
2023-05-08 13:59:21 +0100

    modificacion del login2

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,38 +3,35 @@ import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
-
 const Login = () => {
   const {
     register,
-    // handleSubmit,
-    setValue,
+    handleSubmit,
     formState: { errors },
   } = useForm();
 
   const navegar = useNavigate();
 
   const gestorFormulario = async (data) => {
-   
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const localStorageData = localStorage.getItem("datosUsuario");
-      const usuario = localStorageData
-        ? JSON.parse(localStorageData).userId
-        : null;
-      const token = localStorageData ? JSON.parse(localStorageData).token : null;
-      console.log(usuario);
+    const localStorageData = localStorage.getItem("datosUsuario");
+    const usuario = localStorageData
+      ? JSON.parse(localStorageData).userId
+      : null;
+    const token = localStorageData ? JSON.parse(localStorageData).token : null;
+    console.log(usuario);
 
-     axios
-      .post(process.env.REACT_APP_BACKEND_URL + "/usuarios/login", {
-        email: data.email,
-        password: data.password,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
+    axios
+      .post(
+        process.env.REACT_APP_BACKEND_URL + "/usuarios/login",
+        {
+          email: data.email,
+          password: data.password,
         },
-      }
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
       )
       .then((response) => {
         console.log("Login Correcto");
@@ -52,6 +49,7 @@ const Login = () => {
         console.log(error);
       });
   };
+
   return (
     <div className="Form">
       <div className="title">Accede a tu cuenta</div>
@@ -100,5 +98,112 @@ const Login = () => {
     </div>
   );
 };
-};
+
 export default Login;
+
+
+
+// import React from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useForm } from "react-hook-form";
+// import axios from "axios";
+
+
+// const Login = () => {
+//   const {
+//     register,
+//     handleSubmit,
+//     setValue,
+//     formState: { errors },
+//   } = useForm();
+
+//   const navegar = useNavigate();
+
+//   const gestorFormulario = async (data) => {
+   
+//     const handleSubmit = (event) => {
+//       event.preventDefault();
+//       const localStorageData = localStorage.getItem("datosUsuario");
+//       const usuario = localStorageData
+//         ? JSON.parse(localStorageData).userId
+//         : null;
+//       const token = localStorageData ? JSON.parse(localStorageData).token : null;
+//       console.log(usuario);
+
+//      axios
+//       .post(process.env.REACT_APP_BACKEND_URL + "/usuarios/login", {
+//         email: data.email,
+//         password: data.password,
+//       },
+//       {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       }
+//       )
+//       .then((response) => {
+//         console.log("Login Correcto");
+//         console.log(response.data);
+//         localStorage.setItem(
+//           "datosUsuario",
+//           JSON.stringify({
+//             userId: response.data.userId,
+//             token: response.data.token,
+//           })
+//         );
+//         navegar("/");
+//       })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+//   };
+//   return (
+//     <div className="Form">
+//       <div className="title">Accede a tu cuenta</div>
+//       <div className="inputs">
+//         <form className="formita" onSubmit={handleSubmit(gestorFormulario)}>
+//           <div>
+//             <input
+//               type="text"
+//               name="email"
+//               placeholder="email@email.com"
+//               {...register(
+//                 "email",
+//                 {
+//                   pattern:
+//                     /^(?![_.-])((?![_.-][_.-])[a-zA-Z\d_.-]){0,63}[a-zA-Z\d]@((?!-)((?!--)[a-zA-Z\d-]){0,63}[a-zA-Z\d]\.){1,2}([a-zA-Z]{2,14}\.)?[a-zA-Z]{2,14}$/,
+//                 },
+//                 { required: true, message: "Requerido" }
+//               )}
+//             />
+//             {errors.email && errors.email.type === "required" && (
+//               <span> Campo requerido</span>
+//             )}
+//             {errors.email && errors.email.type === "pattern" && (
+//               <span> Formato incorrecto</span>
+//             )}
+//           </div>
+//           <input
+//             type="password"
+//             name="password"
+//             placeholder="Contraseña"
+//             {...register("password", {
+//               required: true,
+//               minLength: 6,
+//               message: "Requerido",
+//             })}
+//           />
+//           {errors.password && errors.password.type === "required" && (
+//             <span> Campo requerido</span>
+//           )}
+//           {errors.password && errors.password.type === "minLength" && (
+//             <span>Mínimo de 6 caracteres</span>
+//           )}
+//           <button type="submit">"ACCESO"</button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+// };
+// export default Login;
